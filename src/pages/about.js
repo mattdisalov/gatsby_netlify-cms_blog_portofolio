540305cf688f9f36cfaf879de6cb31ee15c2ca7c
Matt Disalov <61394693+mattdisalov@users.noreply.github.com>
2021-03-02 19:20:47 -0500

    fixed bugs

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ const AboutPage = () => {
         <Layout>
             <Head title="About"/>
             <h1>About</h1>
-            <p>This is where the bio will show up later on when I add it.</p>
+            <p>Check out my portfolio on <Link target="_blank" to="https://www.mattdisalov.com">mattdisalov.com</Link></p>
             <p>Need a developer? <Link to="/contact">Contact Me</Link></p>
         </Layout>
     )
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,8 +9,9 @@ const ContactPage = () => {
             <h1>Contact</h1>
             <p>Here's how you can get in touch with me.</p>
             <p><a href="mailto: mattdisalov@gmail.com" target="_blank" rel="noreferrer">E-mail me</a></p>
-            <p><a href="https://www.linkedin.com/in/matt-disalov-107963197/" target="_blank" rel="noreferrer">Linkedin</a></p>
+            <p><a href="https://www.linkedin.com/in/mattdisalov/" target="_blank" rel="noreferrer">Linkedin</a></p>
             <p><a href="https://github.com/mattdisalov" target="_blank" rel="noreferrer">Github</a></p>
+            <p><a href="https://www.mattdisalov.com" target="_blank" rel="noreferrer">Portfolio</a></p>
         </Layout>
     )
 }
